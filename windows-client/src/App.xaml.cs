36f5940793e0adcd5e154cfce8705dfca806d480
ledig11 <ledig6006@gmail.com>
2026-03-22 10:33:16 +0800

    chore: validate MVP v1 end-to-end on Windows

--- windows-client/src/App.xaml.cs
+++ windows-client/src/App.xaml.cs
@@ -1,7 +1,15 @@
 using System.Windows;
 
-namespace WindowsStepGuide.Client;
-
-public partial class App : Application
+namespace WindowsStepGuide.Client
 {
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var window = new MainWindow();
+            window.Show();
+        }
+    }
 }
